ae52a05b54d780ba4b15a43a0e481f1bbbe5673e
PhotobucketSprint1 <bollapuharshitha2001@gmail.com>
2023-03-04 15:55:32 +0530

    user registration test case

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import NavigationBar from "../Navigationbar";
 import axios from "axios"
+import SuccessMsg from "../../views/SuccessMsg";
 
 function Register(){
 
@@ -18,6 +19,7 @@ function Register(){
     const [ passwordValidation, setPasswordValidation ] = useState();
     const [ emailValidation, setEmailValidation ] = useState();
     const [ fileV, setFileV ] = useState();
+    const [ success, setSuccess ] = useState(true);
 
     function handleUserName(eve){
         setUserName(eve.target.value);
@@ -37,18 +39,23 @@ function Register(){
         
         if(userName == undefined){
             setUserNameValidation("Please Enter Username");
+            return;
         }else if(userName.length < 3){
             setUserNameValidation("User name should be greater than 3");
+            return;
          }
 
         if(email == undefined){
             setEmailValidation("Please Enter Email");
+            return;
         }
 
         if(password == undefined){
             setPasswordValidation("Please Enter Password");
+            return;
         }else if(password.length < 3){
             setPasswordValidation("Password should be greater than 3");
+            return;
         }
         var formData = new FormData();
         formData.append("userName", userName);
@@ -62,10 +69,12 @@ function Register(){
               'Content-Type': 'multipart/form-data',
             },
           };
-        axios.post("http://localhost:8080/user/registerUser",formData,config).then((e)=>{
+            axios.post("http://localhost:8080/user/registerUser",formData,config).then((e)=>{
            
                 console.log(e.data)
-                setMessage("User Added Successfully ");
+                setMessage("User Added Successfully");
+                setSuccess(false);
+                console.log(message);
             // }
         }).catch((e)=>{
             console.log(e.data)
@@ -79,9 +88,8 @@ function Register(){
         const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
         if (postImg && allowedTypes.includes(postImg.type)) {
             setFile(postImg)
-            console.log("inside handleimg"+file);
         }else {
-            // setFile(null);
+            
             setFileV('Please select a valid image file (JPEG, PNG, or GIF)');
             
           }
@@ -90,30 +98,31 @@ function Register(){
 
 
     return(
-        <div>
+        <div>   
             <NavigationBar />
+            <SuccessMsg msg="User Added" status={success} />
             <div className="container" style={{marginTop:"100px", marginLeft : "350px"}}>
       <form action="" method="POST" encType="multipart/form-data">
       <div className="col-xs-3">
       <div className="form-group col-md-6">
           <label >Username</label><br/>
-          <input type="text" data-testid="titleTest" className="form-control" id="title" name="title" required onChange={(e)=>{
+          <input type="text" data-testid="usernameTest" className="form-control" id="title" name="title" required onChange={(e)=>{
                     setUserName(e.target.value);
                 }} />
         </div>
-        <p style={{color:'red'}}>{userNameValidation}</p>
+        <p style={{color:'red'}} data-testid="invalidUsernameTest">{userNameValidation}</p>
       </div>
         
         <div className="form-group col-md-6">
           <label >Password</label><br/>
-          <input type="password" className="form-control" data-testid="descriptionTest" id="description" required onChange={(e)=>{
+          <input type="password" className="form-control" data-testid="passwordTest" id="description" required onChange={(e)=>{
                     setPassword(e.target.value);
                  }} />
         </div>
         <p style={{color:'red'}}>{passwordValidation}</p>
         <div className="form-group col-md-6">
           <label >Email Id</label><br/>
-          <input type="email" data-testid="titleTest" className="form-control" required onChange={(e)=>{
+          <input type="email" data-testid="emailTest" className="form-control" required onChange={(e)=>{
                     setEmail(e.target.value);
                 }} />
         </div>
@@ -123,8 +132,8 @@ function Register(){
           <input className="form-control" data-testid="imageTest" accept="image/jpeg,image/png,image/gif" type="file" onChange={handleImg} />
         </div> 
         <p style={{color:'red'}}>{fileV}</p>
-        <button type="submit" className="btn btn-primary" data-testid="savePostBtn" onClick={handleButton}>Register</button>
-        <p style={{color:'green'}}>{message}</p>
+        <button type="submit" className="btn btn-primary" data-testid="registerBtnTest" onClick={handleButton}>Register</button>
+        
       </form>
       </div>
         </div>
