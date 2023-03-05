d455603c10e531e3006f292ffb83d2a503f49295
PhotobucketSprint1 <bollapuharshitha2001@gmail.com>
2023-03-05 20:26:13 +0530

    list of uesr except logged in user

--- src/views/FriendCard.js
+++ src/views/FriendCard.js
@@ -1,18 +1,22 @@
 import pic from "../images/man.png"
 
 function FriendCard(props){
+    const userid = sessionStorage.getItem("userid");
 
     return(
         <div>
-            <div>
-            <div className="card" style={{backgroundColor:"#F0FFFC"}}>
-            <div className="card-body">
-                <h5 className="card-title">{props.username}</h5>
-                <img src={pic} alt="Post image" width="150px" height="150px" />
-                <button className="btn btn-success" >Send Request</button>&nbsp; &nbsp;
+            {
+                props.id == userid ? <></> : <div>
+                <div className="card" style={{backgroundColor:"#F0FFFC"}}>
+                <div className="card-body">
+                    <h5 className="card-title">{props.username}</h5>
+                    <img src={pic} alt="Post image" width="150px" height="150px" />
+                    <button className="btn btn-success" >Send Request</button>&nbsp; &nbsp;
+                </div>
             </div>
         </div>
-    </div>
+        }
+            
 </div>
     )
 }
