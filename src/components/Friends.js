fc56cfbd77eaedb88e8739eca8e8ddbe592a38e1
PhotobucketSprint1 <bollapuharshitha2001@gmail.com>
2023-03-05 19:43:23 +0530

    friend req btn

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -1,9 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ErrorAlertMsg from "../views/ErrorAlertMsg";
+import FriendCard from "../views/FriendCard";
 
 
 function Friends(){
-    return (<div>
-        <h4>User Can See All Followers</h4>
-    </div>)
+
+    const [ users, setUsers ] = useState([]);
+    const [ images, setImages ] = useState([]);
+
+    useEffect(()=>{
+        axios.get(`http://localhost:8080/admin/allUser`)
+            .then((res)=>{
+                console.log(res.data);
+                setUsers(res.data);
+            })
+            .catch((err)=>{
+                console.log(err);
+            })
+
+        axios.get(`http://localhost:8080/admin/allUserImages`)
+            .then((res)=>{
+                console.log(res.data);
+                setImages(res.data);
+            })
+            .catch((err)=>{
+                console.log(err);
+            })
+
+    },[]);
+
+    return (
+        <div className="container">
+            
+            <div className="row">
+            {
+                users.length > 0 ? users.map((user)=>{
+                    return <div class="col-md-4"><FriendCard id={user.id} username={user.userName} /></div>
+                }) : <ErrorAlertMsg msg="You have no Friends !" status={false} />
+                
+            }
+            </div>
+        </div>
+    )
 }
 
 export default Friends;
---
+++ src/views/FriendCard.js
@@ -0,0 +1,19 @@
+import pic from "../images/man.png"
+
+function FriendCard(props){
+
+    return(
+        <div>
+            <div>
+            <div className="card" style={{backgroundColor:"#F0FFFC"}}>
+            <div className="card-body">
+                <h5 className="card-title">{props.username}</h5>
+                <img src={pic} alt="Post image" width="150px" height="150px" />
+                <button className="btn btn-success" >Send Request</button>&nbsp; &nbsp;
+            </div>
+        </div>
+    </div>
+</div>
+    )
+}
+export default FriendCard;
